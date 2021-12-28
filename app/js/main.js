5e0556c31ab2219700cb3ea749100987cbf2a781
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-12-28 20:55:30 +0200

    add slider

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".slider__inner").slick({
+    prevArrow:
+      '<button class="slick-btn slick-prev"><img src="./images/icon-arrow-left.png" alt="prev"></button>',
+    nextArrow:
+      '<button class="slick-btn slick-next"><img src="./images/icon-arrow-right.png" alt="next"></button>',
+  });
+});
